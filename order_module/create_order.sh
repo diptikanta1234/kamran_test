e7a8eb148480bd3d414137e4e198fc19716ac519
diptikanta1234 <pradhandiptikanta@gmail.com>
2024-05-07 22:49:49 +0530

    added vss project details

--- order_module/create_order.sh
+++ order_module/create_order.sh
@@ -1,1 +1,14 @@
 echo ""[Ddisplay list of orders[B"
+echo "the DM of this project is Abhi"
+
+
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
+echo "not intested in this project"
